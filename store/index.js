4628d49415b1f4c43563ec3b2c8cdf6416d77927
marco94gug <marco.guglielmino94@gmail.com>
2022-09-26 13:20:50 +0200

    feat: introducing first version of redux logic

--- store/index.js
+++ store/index.js
@@ -1,54 +1,35 @@
 import { combineReducers, createStore } from "redux";
 
 const initialState = {
-  cartData: {
-    cartList: [],
-  },
-  activities: {},
-  cities: {
-    cityData: {}
-  }
+    cartData: {
+        cartList: [],
+    },
+    activities: {
+
+    }
 };
 
 function cartDataReducer(state = {}, action) {
-  switch (action.type) {
-    case "ADD_PRODUCT":
-      return { ...state, cartList: [...state.cartList, action.payload] };
-    case "REMOVE_PRODUCT":
-      console.log(action.payload);
-      return {
-        ...state,
-        cartList: state.cartList.filter((_, id) => id !== action.payload),
-      };
-    default:
-      return state;
-  }
-}
-
-function activitiesReducer(state = {}, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
-
-function cityReducer(state = {}, action) {
     switch (action.type) {
-        case "SET_CITY":
-            return {cityData: action.payload};
-        default:
+        case 'ADD_PRODUCT':
+            return {...state, cartList: [...state.cartList, action.payload]};
+        case 'REMOVE_PRODUCT':
+            console.log(action.payload)
+            return {...state, cartList: state.cartList.filter((_, id) => id !== action.payload)};
+        default: 
             return state;
     }
-}
+};
 
-const rootReducer = combineReducers({
-  cartData: cartDataReducer,
-  activities: activitiesReducer,
-  cities: cityReducer,
-});
+function activitiesReducer(state = {}, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+};
+
+const rootReducer = combineReducers({cartData: cartDataReducer, activities: activitiesReducer});
 
 const store = createStore(rootReducer, initialState);
 
 export default store;
-
-//test second
