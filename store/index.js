f2af926e1cb5a9306af5aa40c8adcf8a8e4c08be
marco94gug <marco.guglielmino94@gmail.com>
2022-09-26 14:17:32 +0200

    chore: test commit

--- store/index.js
+++ store/index.js
@@ -1,35 +1,41 @@
 import { combineReducers, createStore } from "redux";
 
 const initialState = {
-    cartData: {
-        cartList: [],
-    },
-    activities: {
-
-    }
+  cartData: {
+    cartList: [],
+  },
+  activities: {},
 };
 
 function cartDataReducer(state = {}, action) {
-    switch (action.type) {
-        case 'ADD_PRODUCT':
-            return {...state, cartList: [...state.cartList, action.payload]};
-        case 'REMOVE_PRODUCT':
-            console.log(action.payload)
-            return {...state, cartList: state.cartList.filter((_, id) => id !== action.payload)};
-        default: 
-            return state;
-    }
-};
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return { ...state, cartList: [...state.cartList, action.payload] };
+    case "REMOVE_PRODUCT":
+      console.log(action.payload);
+      return {
+        ...state,
+        cartList: state.cartList.filter((_, id) => id !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
 
 function activitiesReducer(state = {}, action) {
-    switch (action.type) {
-        default:
-            return state
-    }
-};
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
 
-const rootReducer = combineReducers({cartData: cartDataReducer, activities: activitiesReducer});
+const rootReducer = combineReducers({
+  cartData: cartDataReducer,
+  activities: activitiesReducer,
+});
 
 const store = createStore(rootReducer, initialState);
 
 export default store;
+
+//test
