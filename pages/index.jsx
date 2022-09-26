167c1a59a8beb0ca349c5b98b06847fdc79f8916
marco94gug <marco.guglielmino94@gmail.com>
2022-09-26 16:40:51 +0200

    feat: updated Redux functions, added Base URL

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,29 +2,34 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState} from 'react';
+import useFetch  from '../utils/useFetch/useFetch';
+import { IMPORT_URL } from '../utils/useFetch/URL';
 
 export default function Home() {
+  // const dispatch = useDispatch();
+  // const {cartData, cities} = useSelector(state => state)
 
-  const {cartData} = useSelector(state => state)
+  // useEffect(() => {
+  //     useFetch(IMPORT_URL.CITIES, 1, dispatch, "SET_CITY" )
+  // }, [])
 
-  const dispatch = useDispatch();
+  // console.log(cartData)
+  // const handleOnClick = () => {
+  //   console.log('cliccato')
+  //   dispatch({type: "ADD_PRODUCT", payload: cities.cityData.name});
+  //   console.log(cities.cityData)
+  // }
 
-  console.log(cartData)
-
-  const handleOnClick = () => {
-    console.log('cliccato')
-    dispatch({type: "ADD_PRODUCT", payload: "Catania"});
-  }
-
-  const deleteTest = (id) => {
-    dispatch({type: "REMOVE_PRODUCT", payload: id});
-  }
+  // const deleteTest = (id) => {
+  //   dispatch({type: "REMOVE_PRODUCT", payload: id});
+  // }
 
   return (
     <div className={styles.container}>
-      <h1>Prova</h1>
+      {/* <h1>Prova</h1>
       <button onClick={handleOnClick}>Add</button>
-      {cartData.cartList.map((el, index) => <li onClick={() => deleteTest(index)}>{el}</li>)}
+      {cartData.cartList.map((el, index) => <li onClick={() => deleteTest(index)}>{el}</li>)} */}
      
     </div>
   )
