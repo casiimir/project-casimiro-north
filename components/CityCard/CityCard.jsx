bb59fbbf0504d2cf9941544365441a45c16955ea
SeferMetatron <mistificatorlol@gmail.com>
2022-09-27 17:07:45 +0200

    feat: first card implementation + mixins

--- components/CityCard/CityCard.jsx
+++ components/CityCard/CityCard.jsx
@@ -1,12 +1,29 @@
 import styles from "./index.module.scss";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { useState } from "react";
+import Link from "next/link";
 
-const CityCard = () => {
+const CityCard = ({ data }) => {
+  const [fav, setFav] = useState();
+  const onBtnclick = () => {};
   return (
     <div className={styles.CityCard}>
-      <img src="https://picsum.photos/200/300" alt="CityPhoto" />
-      <h3>Nomi</h3>
-      <p>Cose</p>
-      <button>Città</button>
+      <img
+        className={styles.Image}
+        src="https://picsum.photos/200/300"
+        alt="CityPhoto"
+      />
+      <div className={styles.Content}>
+        <h3 className={styles.Text}>Nomi</h3>
+        {/* <div>
+          <span>_____</span>
+        </div> */}
+        <p className={styles.Description}>Cose</p>
+      </div>
+
+      <button className={styles.Button} onClick={onBtnclick}>
+        <FaRegHeart />
+      </button>
     </div>
   );
 };
--- components/CityCardList/CityCardList.jsx
+++ components/CityCardList/CityCardList.jsx
@@ -1,10 +1,19 @@
 import styles from "./index.module.scss";
 import CityCard from "../CityCard/CityCard";
+import { useState, useEffect } from "react";
 
 const CityCardList = () => {
   return (
-    <div className={styles.Footer}>
-      <CityCard />
+    <div className={styles.CityCardList}>
+      <h2 className={styles.Header}>Top Cities</h2>
+      <div className={styles.List}>
+        <CityCard />
+        <CityCard />
+        <CityCard />
+        <CityCard />
+        <CityCard />
+        <CityCard />
+      </div>
     </div>
   );
 };
