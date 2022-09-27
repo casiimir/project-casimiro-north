f67bef9bcea684edb2b27ecf98bdff1b1e28ee2c
SeferMetatron <mistificatorlol@gmail.com>
2022-09-27 10:09:58 +0200

    feat: CityCard Init

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import CityCardList from "../components/CityCardList";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.Home}>
-      
-
-      
+      <CityCardList />
     </div>
-  )
+  );
 }
