49379252490f32a886288fe61aa1348340ec2dfa
marco94gug <marco.guglielmino94@gmail.com>
2022-09-28 16:58:28 +0200

    feat: upgraded homepage

--- utils/useFetch/URL.js
+++ utils/useFetch/URL.js
@@ -2,5 +2,8 @@ export const IMPORT_URL = {
     BASE_URL: "https://sandbox.musement.com/api/v3/",
     get CITIES () {
         return this.BASE_URL + "cities/"
+    },
+    get ACTIVITIES () {
+        return this.BASE_URL + "activities/"
     }
 }
