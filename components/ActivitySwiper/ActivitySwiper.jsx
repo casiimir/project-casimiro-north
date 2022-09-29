e89a057994fef8487c94e67dc2f7b983c2d4fd2c
marco94gug <marco.guglielmino94@gmail.com>
2022-09-29 14:15:03 +0200

    feat: finish swiper component

--- components/ActivitySwiper/ActivitySwiper.jsx
+++ components/ActivitySwiper/ActivitySwiper.jsx
@@ -2,26 +2,53 @@ import styles from './index.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {Navigation, Pagination} from 'swiper';
 import 'swiper/css';
+import "swiper/css/navigation";
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import GET from '../../utils/GET/GET';
+import { IMPORT_URL } from '../../utils/GET/URL';
 
 export default function ActivitySwiper () {
+
+  const { activities } = useSelector((state) => state);
+  const rotuer = useRouter();
+  const dispatch = useDispatch();
+  const { cityname } = rotuer.query
+
+  console.log(activities.activitiesTodayList)
+
+  useEffect(()=>{
+    cityname?.split("&=")[1] &&
+      GET(IMPORT_URL.CITIES, `${cityname?.split("&=")[1]}/activities?limit=10`, dispatch, "SET_TODAY_ACTIVITIES")
+  }, [])
+
+  
+
     return (
       <div>
-        <Swiper className={styles.mySwiper}
+        <Swiper 
+        className='myswiper'
+        modules={[Navigation]}
+        navigation={true}
         style={{
           "--swiper-navigation-color": "#fff",
           "--swiper-pagination-color": "#fff",
         }}
-        navigation={true}
-        modules={[Navigation, Pagination]}
-        slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-        >
-        <SwiperSlide><img src="https://picsum.photos/300/200" alt="img" /> </SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/300/200" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/300/200" alt="img" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/300/200" alt="img" /></SwiperSlide>
+         >
+          {!activities.activitiesTodayList.message && 
+            activities?.activitiesTodayList?.data?.map((item, index) =>
+              <SwiperSlide key={index}>
+                <div className={styles.swiper_container}>
+                    <img src={item.cover_image_url} alt="activity" /> 
+                    <h3>{item.title}</h3>
+                </div>
+              </SwiperSlide>
+          )}
+          
         </Swiper>
       </div>
     );
   };
+
+  // https://api.musement.com/api/v3/cities/{cityId}/activities/today
