3986fd676dc53ec0632f161cdfe90a1b7f9da5a2
marco94gug <marco.guglielmino94@gmail.com>
2022-09-30 11:49:54 +0200

    chore: init commit

---
+++ pages/activity/index.jsx
@@ -0,0 +1,1 @@
+// pagina che ti reindirizza
--- pages/city/[cityname]/category/[categoryname].jsx
+++ pages/city/[cityname]/category/[categoryname].jsx
@@ -1,6 +1,8 @@
+import styles from './category.module.scss';
+
 export default function CategoryPage () {
 
     return (
-        <div>CATEGORY PAGE</div>
+        <div className={styles.CategoryPage}>CATEGORY PAGE</div>
     )
 }
