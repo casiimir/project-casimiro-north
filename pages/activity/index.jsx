06fee2202d3c253d1b198472a1ea9bf17b9a61a9
marco94gug <marco.guglielmino94@gmail.com>
2022-09-30 20:48:55 +0200

    fix: fixed vercel bugs

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -1,1 +1,17 @@
 // pagina che ti reindirizza
+import { useRouter } from "next/router"
+import { useLayoutEffect } from "react";
+
+export default function ActivityRe() {
+
+
+    const router = useRouter();
+
+    useLayoutEffect(() => {
+        router.back();
+    }, []) 
+
+    return (
+        <div></div>
+    )
+}
