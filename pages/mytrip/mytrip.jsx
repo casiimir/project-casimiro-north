b3959f5cbd49eee4215843cf04fdff80b6cb9720
elerizz <eleonorarizzitello@gmail.com>
2022-09-30 16:27:18 +0200

    feat: added list in mytrip page

--- pages/mytrip/mytrip.jsx
+++ pages/mytrip/mytrip.jsx
@@ -6,13 +6,23 @@ import { IMPORT_URL } from "../../utils/GET/URL";
 import Link from "next/link";
 
 export default function MyTripPage() {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state);
+  // const router = useRouter();
+  // const dispatch = useDispatch();
+  // const data = useSelector((state) => state);
 
   return (
     <div className={styles.MyTrip}>
-      <h2 className={styles.header}> My trip in <h2 className={styles.city}>Palermo</h2></h2>
+      <div className={styles.container}>
+        <h2 className={styles.header}>My trip in <b className={styles.city}>PALERMO</b></h2>
+        <div className={styles.row}/> 
+
+        <ul className={styles.activities}>
+          <li className={styles.activitiy}> ARTE E CULTURA</li>
+          <li className={styles.activitiy}> TOUR</li>
+          <li className={styles.activitiy}> BY NIGHT</li>
+          <li className={styles.activitiy}> ADVENTURES</li>
+        </ul>
+      </div>
     </div>
   );
 }
