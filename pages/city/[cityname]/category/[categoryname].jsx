87db0c2492c459b3c5107c84510d248b6f6935d8
marco94gug <marco.guglielmino94@gmail.com>
2022-09-30 13:14:54 +0200

    feat: complete activity card on category per city page - mobile ver

--- pages/city/[cityname]/category/[categoryname].jsx
+++ pages/city/[cityname]/category/[categoryname].jsx
@@ -1,8 +1,11 @@
 import styles from './category.module.scss';
+import ActivityCardList from '../../../../components/ActivityCardList/ActivityCardList';
 
 export default function CategoryPage () {
 
     return (
-        <div className={styles.CategoryPage}>CATEGORY PAGE</div>
+        <div className={styles.CategoryPage}>
+            <ActivityCardList />
+        </div>
     )
 }
