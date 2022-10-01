15dd47e1ac2eaa8d0cb61b15fe60fb117d874404
Waterlilyrr <noemict94@hotmail.it>
2022-10-01 22:32:02 +0200

    feat: aboutUsPage style set

--- pages/aboutus/aboutus.jsx
+++ pages/aboutus/aboutus.jsx
@@ -10,5 +10,28 @@ export default function AboutUsPage() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state);
 
-  return <div className={styles.aboutUsPage}></div>;
+  return (
+    <div className={styles.aboutUsPage}>
+      <div className={styles.mainSection}>
+        <div className={styles.infoSection}>
+          <div className={styles.title}>
+            <h1 className={styles.titleText}>ABOUT US</h1>
+          </div>
+          <p className={styles.description}>
+            breve descrizione di quanto siamo bravi ❤️
+          </p>
+          <div className={styles.info}>
+            <span className={styles.logoForse}>logo(credo)</span>
+            <span className={styles.githubLink}>Github repository link</span>
+          </div>
+        </div>
+        <div className={styles.swiper}>qui andrebbe lo swiper</div>
+
+        <div className={styles.creditsSection}>
+          <img src="" alt="logoPolarix" className={styles.logo} />
+          <h4 className={styles.creditsText}>made to always be with you</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
