25b79ddbb720354615b0b6f28f5a4216a77cae5e
marco94gug <marco.guglielmino94@gmail.com>
2022-10-01 16:39:54 +0200

    feat: finish cart style modal

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -1,15 +1,17 @@
 import styles from "./index.module.scss";
 import { BsCart2, BsCheckCircleFill } from "react-icons/bs";
+import ModalIcon from "./modalIcon";
 
 const Modal = ({ isVisibile, onModalClick }) => {
   return (
     isVisibile && (
-      <div className={styles.modaloverlay} onClick={() => onModalClick(false)}>
+      <div className={styles.modaloverlay}>
+        <div className={styles.overlay} onClick={() => onModalClick(false)}/>
         <div className={styles.modal}>
           <h2 className={styles.mainText}>ADDED TO CART!</h2>
-
-          <BsCart2 className={styles.cart} />
-          <BsCheckCircleFill className={styles.check} />
+          <div className={styles.cart_icon}>
+          <ModalIcon className={styles.carrello}/>
+          </div>
         </div>
       </div>
     )
