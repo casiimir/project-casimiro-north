d89ae7cf946259545352d9b0e0447c4aa9d68b12
elerizz <eleonorarizzitello@gmail.com>
2022-10-02 13:40:44 +0200

    :feat: styled newsletter

--- components/Newsletter/Newsletter.jsx
+++ components/Newsletter/Newsletter.jsx
@@ -1,19 +1,21 @@
-import "./index.module.scss";
+import styles from "./index.module.scss";
 
 const Newsletter = () => {
     return(
-        <div >
+        <div className={styles.newsletter}>
                 <h2>NEWSLETTER</h2>
-                <h5>Take a 10% coupon subscribing to our Newsletter!</h5> 
-                <form action="submit" >   
-                    <div>
+                <p>Take a <b className={styles.coupon}>10% coupon</b> subscribing to our Newsletter!</p> 
+                <div className={styles.container}>
+                <form action="submit" className={styles.form}>   
+                    <div className={styles.fullname}>
                         <input type="text" placeholder="Name" />
-                        <input type="text" placeholder="Surame" />
+                        <input type="text" placeholder="Surname" />
                     </div>
-                    <input type="text" placeholder="Email"  />
-                    <button>Submit</button>
+                    <input type="text" placeholder="Email"  className={styles.email}/>
+                    <button>SUBMIT</button>
                 </form>                    
                 <img src="../assets/newsletter.png" alt="newsletter"/>
+                </div>
         </div>
     )
 }
