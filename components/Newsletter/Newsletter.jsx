7af53340fe5650247a0405628b81f2de120a76b0
marco94gug <marco.guglielmino94@gmail.com>
2022-10-02 21:32:31 +0200

    feat: clean and finish styling for newsletter component

--- components/Newsletter/Newsletter.jsx
+++ components/Newsletter/Newsletter.jsx
@@ -1,10 +1,13 @@
 import styles from "./index.module.scss";
+import newsLetterImg from '../../assets/newsletter.png';
 
 const Newsletter = () => {
     return(
         <div className={styles.newsletter}>
+            <div className={styles.newsletter_main_title}>
                 <h2>NEWSLETTER</h2>
-                <p>Take a <b className={styles.coupon}>10% coupon</b> subscribing to our Newsletter!</p> 
+                <p>Take a <b className={styles.coupon}>10% coupon</b> subscribing to our Newsletter!</p>
+            </div> 
                 <div className={styles.container}>
                 <form action="submit" className={styles.form}>   
                     <div className={styles.fullname}>
@@ -14,7 +17,7 @@ const Newsletter = () => {
                     <input type="text" placeholder="Email"  className={styles.email}/>
                     <button>SUBMIT</button>
                 </form>                    
-                <img src="../assets/newsletter.png" alt="newsletter"/>
+                <img src={newsLetterImg.src} alt="newsletter"/>
                 </div>
         </div>
     )
