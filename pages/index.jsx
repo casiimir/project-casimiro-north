fefa3fe849adb638d765994c63aef6ae685c1e26
marco94gug <marco.guglielmino94@gmail.com>
2022-10-04 22:17:33 +0200

    feat: introducing loading page

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import GET from "../utils/GET/GET";
 import { IMPORT_URL } from "../utils/GET/URL";
-import star from "../public/star.png";
-import polari from "../public/polari.png";
+
 import Hero from "../components/Hero";
 import MiniCarousel from "../components/MiniCarousel";
 import CityCardList from "../components/CityCardList";
@@ -31,31 +30,11 @@ export default function Home() {
   // const deleteTest = (id) => {
   //   dispatch({type: "REMOVE_PRODUCT", payload: id});
   // }
-  const [loadVisible, setLoadVisible] = useState(true);
-  const [status, setStatus] = useState("");
-
-  useEffect(() => {
-    setStatus("disactive");
-    if (loadVisible) {
-      setTimeout(() => {
-        setLoadVisible(false);
-      }, 4200);
-    }
-  }, [loadVisible]);
+  
 
   return (
     <div className={styles.Home}>
-      {loadVisible && (
-        <div
-          className={`${styles.overlayanimation} ${styles[status]}`}
-          loadVisible={loadVisible}
-          setLoadVisible={setLoadVisible}
-        >
-          <img src={star.src} className={styles.star} alt="starX" />{" "}
-          <img src={polari.src} className={styles.polari} alt="polari" />{" "}
-        </div>
-      )}
-      <Hero type="Home" />
+      
       {/* <button onClick={() => console.log(cities.cityList)}>console.log</button> */}
       {/* <h1>Prova</h1>
       <button onClick={handleOnClick}>Add</button>
