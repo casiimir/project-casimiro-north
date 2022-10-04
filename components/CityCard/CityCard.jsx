63ffaa5403dd2dd5ba281ac4c569a3eb5ff83d10
marco94gug <marco.guglielmino94@gmail.com>
2022-10-04 12:28:38 +0200

    feat: create shimmer.js file on utils

--- components/CityCard/CityCard.jsx
+++ components/CityCard/CityCard.jsx
@@ -3,28 +3,9 @@ import { FaRegHeart } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import Placeholder from '../../assets/placeholder.gif'
+import { toBase64, shimmer } from "../../utils/shimmer";
 
 const CityCard = ({ data }) => {
-
-  const shimmer = (w, h) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-const toBase64 = (str) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
 
   const { name, cover_image_url, activities_count, id } = data;
   const router = useRouter();
